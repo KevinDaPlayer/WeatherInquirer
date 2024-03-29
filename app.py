80efb11255eb1ffc04ce21b59b5e265e9de148d9
KevinDaPlayer <325881@gmail.com>
2024-03-29 20:01:51 +0800

    weatherRequest

--- app.py
+++ app.py
@@ -1,9 +1,24 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import requests
 
 app = Flask(__name__)
+
+Weather_APIKEY = '779701edbbc768ee1f879cc1b8d2baaf'
+
 @app.route('/')
 def HelloWorld():
     return '你好世界'
+
+@app.route('/weather', methods=['GET'])
+def get_weather():
+    city = request.args.get('city')
+    if not city:
+        return jsonify({'error':'Missing city parameter'})
+    url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={Weather_APIKEY}&units=metric'
+
+    response = requests.get(url)
+    return jsonify(response.json())
+
 
 if __name__ == '__main__':
     app.run(debug=True)
